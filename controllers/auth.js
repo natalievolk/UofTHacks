aa1fb2b88588bcbc1205e5b80efec27345d2e1d6
qingyuan-wu <qyw.wu@mail.utoronto.ca>
2022-02-05 17:18:05 -0500

    added register, connected to db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,61 @@
-//register the user
+//handle register the user
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs'); //password hasing
+
+const mysql = require('mysql');
+var db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
 
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send("form submitted");
+    // sample req.body
+    //{
+    //   name: 'bob',
+    //   email: 'bob123@mail.com',
+    //   number: '6471234567',
+    //   password: 'bob123',
+    //   passwordConfirm: 'bob123'
+    // }
+    const { name, email, cell, password, passwordConfirm } = req.body;
+
+    //check duplicates
+    db.query(`
+        SELECT email
+        FROM users
+        WHERE email = ? OR cell = ?
+    `, [email, cell], async (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        else if (results.length > 0) {
+            return res.render('register', {
+                message: 'the email or phone is already in use!'
+            });
+        } else if ( password !== passwordConfirm) {
+            return res.render('register', {
+                message: "passwords don't match!"
+            });
+        } else {
+            let hashedPassword = await bcrypt.hash(password, 8); // 8 is num of rounds
+            console.log(hashedPassword);
+
+            db.query(`INSERT INTO users SET ?`, { name:name, email:email, cell:cell, password:hashedPassword }, (err, results) => {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log(results);
+                    return res.render('register', {
+                        message: 'user registered successfully'
+                    });
+                }
+            })
+        }
+    });
+
+
+    // res.send("form submitted!");
 }
--- notes.md
+++ notes.md
@@ -1,4 +1,6 @@
-install express and dependencies
+# General Notes
+
+* install express and dependencies
 
 ```bash
 npm init -y
@@ -12,9 +14,25 @@ npm install method-override --save
 npm install mysql --save
 npm i dotenv
 npm i nodemon --save # auto refresh
+
+npm i cookie-parser jsonwebtoken # cookies
+
+npm i bcryptjs # for password hashing
 # i think some of them you don't need --save I just saw the tutorial guy do them
 # https://www.youtube.com/watch?v=VavWEtI5T7c 7:25 mark here he downloads stuff
 ```
 
-* 
+* configure `package.json`: add `"start":"nodemon app.js"` in `"scripts"` ->
+
+```json
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start":"nodemon app.js"
+  },
+```
+* This will make sure the scripts are run by nodemon (auto refresh!)
+* `npm start` to run -> navigate to localhost:3001 (or whichever port specified in `app.js`)
 * sometiems when you make changes you must refresh!
+
+## To-do
+* phone number parsing proper form
